53fb1f351111e3854ee1af4f341eb6d405e429f8
Emilio Silva Schlenker <e18r@nietzsche>
2019-04-23 17:49:38 -0500

    Login with WebView

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { WebView } from 'react-native';
 import Main from './Main';
 
-export default class App extends React.Component {
+export default class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      server: 'http://192.168.43.198:8080',
+      loginUri: null,
+      token: null
+    };
+  }
+
+  async componentWillMount() {
+    const { server } = this.state;
+    let response = await fetch(server + '/login');
+    let loginUri = response.url;
+    this.setState({ loginUri });
+  }
+
   render() {
+    const { server, loginUri, token } = this.state;
     return (
-      <Main
-        server='http://192.168.43.198:8080'
-        token='7NULiAGMBEYmiwaJLSxPGStkfItcNGw2iz3i7z6yZtm9FMQj24DYga3hhJc2ZvGF'
+      <WebView
+        source={{uri: loginUri}}
+        style={{marginTop: 20}}
       />
     );
   }
